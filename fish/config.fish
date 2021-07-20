61c3e55be06d7ee4c8ec797ecc0f75e21d8fa7fa
SakuraShoyo <mizzaelsantos1@gmail.com>
2021-07-19 23:51:54 -0300

    added setxkbmap to fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,4 +3,10 @@ if status is-interactive
     status is-interactive; and pyenv init --path | source
     pyenv init - | source
     set -x IDF_PATH ~/esp/esp-idf
+    set fish_greeting
+    
+    ## set keyboard
+    setxkbmap -model abnt2 -layout br -variant abnt2
+
+
 end
