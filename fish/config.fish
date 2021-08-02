cacadcc6c9721ab3c69d3f01cdc5cbc4ad02b53a
SakuraShoyo <mizzaelsantos1@gmail.com>
2021-08-02 13:31:02 -0300

    added rust to path in fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,10 +3,8 @@ if status is-interactive
     status is-interactive; and pyenv init --path | source
     pyenv init - | source
     set -x IDF_PATH ~/esp/esp-idf
+
     set fish_greeting
-    
     ## set keyboard
     setxkbmap -model abnt2 -layout br -variant abnt2
-
-
 end
