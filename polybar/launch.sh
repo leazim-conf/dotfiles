95f94bdf69a27c2842c2d3efacf1d3e094b4a27d
SakuraShoyo <mizzaelsantos1@gmail.com>
2021-08-06 16:50:05 -0300

    added bspwmrc 2 monitors config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,4 +2,13 @@
 
 killall -q polybar 
 
-polybar example
+#wait the process shut down
+#while pgrep -u $UID -x > /dev/null; do sleep 1; done
+
+# Launch bar
+polybar top &
+
+my_laptop_external_monitor=$(xrandr --query | grep 'DP-1')
+if [[ $my_laptop_external_monitor = *connected* ]]; then
+    polybar top_external &
+fi
