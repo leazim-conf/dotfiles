96d6f6c4e3be197d0d0a2b0f89befc1c68d7437c
SakuraShoyo <mizzaelsantos1@gmail.com>
2021-09-19 19:52:57 -0300

    add DWM config, icons and colors

--- sucklesstools/DWM/config.h
+++ sucklesstools/DWM/config.h
@@ -6,24 +6,26 @@ static const unsigned int gappx     = 1;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=11", "Font Awesome 5 Brands:style=Bold:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 
-static const char col_norm[]        = "#09040A";
-static const char col_sel[]         = "#C3C3C5";
+static const char black[]        		= "#09040A";
+static const char light_gray[]      = "#C3C3C5";
+static const char deep_blue[]     	= "#1A1B26";
+static const char blue[]     				= "#7AA2F7";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_norm },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_sel  },
+	/*               fg         	 bg         	border   */
+	[SchemeNorm] = { blue, 				deep_blue, 	 		 black },
+	[SchemeSel]  = { light_gray, 	deep_blue,  light_gray },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = {"", "", "", "", "", "", "6" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -31,8 +33,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",  	NULL,       NULL,  	0,       		0,            2 },
-	{ "Discord",    NULL,       NULL,  	0,       		0,            4 },
+	{ "firefox",  	NULL,       NULL,  				0,       		0,          	2 },
+	{ "discord",    NULL,       NULL,  				0,       		0,            4 },
 };
 
 /* layout(s) */
