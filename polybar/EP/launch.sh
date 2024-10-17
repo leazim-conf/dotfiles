ab59c7c7bf5354afd9492d16bb9fdfde0173d607
frmiza <mizzaelsantos1@gmail.com>
2024-10-16 23:53:54 -0300

    ew polybar

--- polybar/EP/launch.sh
+++ polybar/EP/launch.sh
@@ -3,9 +3,9 @@
 killall -q polybar 
 
 # Launch bar
-polybar -c ~/.config/polybar/simple/config top &
+polybar -c ~/.config/polybar/EP/config top &
 
 my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
 if [[ $my_laptop_external_monitor = *connected* ]]; then
-    polybar top_external &
+    polybar top &
 fi
